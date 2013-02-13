9559dcad6919a79c77af6e7f73b4b40b4501ff69
Ammar Qammaz <ammarkov@gmail.com>
2013-02-13 14:42:23 +0200

    improved priorities etc ,added clang compiler

--- bigtest.sh
+++ bigtest.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
-sudo nice -n 20 ./MatrixTypeConversion KIx30.txt KData30b.txt Out.txt& 
+
+sudo echo "Password" 
+
+if [ $( id -u ) -eq 0 ]; then
+echo "Running as SuperUser , going for full performance"
+
+governor="performance"
+for CPUFREQ in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+do
+[ -f $CPUFREQ ] || continue
+echo -n $governor > $CPUFREQ
+done
+
+  sudo nice -n -20 ionice -c 1 -n 0  time ./MatrixTypeConversion KIx30.txt KData30b.txt Out.txt&
+  exit 0
+
+fi
+
+ 
+echo "Running as normal User"
+time ./MatrixTypeConversion KIx30.txt KData30b.txt Out.txt& 
+
 exit 0
--- make
+++ make
@@ -7,5 +7,8 @@ OPTIMIZATIONST="-march=corei7 -mtune=corei7 -funroll-loops -fomit-frame-pointer 
 OPTIMIZATIONS="-s -O3 -fexpensive-optimizations -march=native -mtune=native"
 
 gcc main.c $OPTIMIZATIONST -o MatrixTypeConversion
+strip MatrixTypeConversion
+
+clang -O3 main.c -o MatrixTypeConversionClang
 
 exit 0
